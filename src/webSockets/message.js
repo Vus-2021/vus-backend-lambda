358cf77de2eb271e297a455c0f717a47376ad96b
younghoon.choi <c_yh0327@naver.com>
2021-04-26 16:09:05 +0900

    [feat] websocket 추가

--- src/webSockets/message.js
+++ src/webSockets/message.js
@@ -1,37 +1,31 @@
-const { get, create } = require('../services/index');
+const AWS = require('aws-sdk');
 const WebSocket = require('../modules/websocketMessage');
 const Responses = require('../modules/socketResponse');
 
 exports.handler = async (event) => {
-    console.log('event', event);
-    const { connectionId: connectionID } = event.requestContext;
-
-    const body = JSON.parse(event.body);
+    console.log('event', JSON.stringify(event, null, 2));
+    const ddb = new AWS.DynamoDB.DocumentClient();
+    let connectionData;
 
     const tableName = process.env.SOCKET_TABLE_NAME;
     try {
-        const { data: record } = await get({
-            ID: connectionID,
-            tableName,
+        connectionData = await ddb
+            .scan({
+                TableName: tableName,
+            })
+            .promise();
+
+        const postCalls = connectionData.Items.map(async (item) => {
+            await WebSocket.send({
+                domainName: item.domainName,
+                stage: item.stage,
+                connectionID: item.ID,
+                message: 'refetch',
+            });
         });
 
-        const { messages, domainName, stage } = record;
+        await Promise.all(postCalls);
 
-        messages.push(body.message);
-
-        const data = {
-            ...record,
-            messages,
-        };
-
-        await create({ ...data, tableName });
-
-        await WebSocket.send({
-            domainName,
-            stage,
-            connectionID,
-            message: 'This is a reply to your message',
-        });
         return Responses._200({ message: 'got a message' });
     } catch (error) {
         return Responses._400({ message: 'message could not be received' });
